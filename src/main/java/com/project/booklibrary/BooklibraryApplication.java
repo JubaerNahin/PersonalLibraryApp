6c119289da5685907c665c272f2d13051e577fdf
Jubaer Islam Nahin <jubaernahin17@gmail.com>
2023-11-20 13:24:00 +0600

    init

--- src/main/java/com/project/booklibrary/BooklibraryApplication.java
+++ src/main/java/com/project/booklibrary/BooklibraryApplication.java
@@ -1,13 +1,10 @@
 package com.project.booklibrary;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import com.project.booklibrary.Entity.Book;
-import com.project.booklibrary.Repository.BookRepository;
+
 
 @SpringBootApplication
 @ComponentScan
@@ -16,14 +13,6 @@ public class BooklibraryApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(BooklibraryApplication.class, args);
 	}
-	@Autowired
-  private BookRepository bookRepository;
-	// @Override
-	// public void run(String... args) throws Exception {
-	// 	Book book1 = new Book("Atomic Habit","James Clear","Non-fiction");
-  //   bookRepository.save(book1);
-	// 		Book book2 = new Book("Atomic Habit","James Clear","Non-fiction");
-  //   bookRepository.save(book2);
-	// }
+	
 
 }
